001f887e79e4dc86c06bfd6b7ca7d74d4b6c87a5
RedwanAhmedTapuu <redwan25880@gmail.com>
2024-06-28 17:04:20 +0600

    videomeeting and createRoom updated

--- src/components/CreateRoom.jsx
+++ src/components/CreateRoom.jsx
@@ -1,24 +1,21 @@
-// CreateRoom.js
 import { useCallback, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSocket } from "../context/SocketProvider";
 import Modal from "react-modal";
-import { FiCopy } from "react-icons/fi";
-import { usePeer } from "../context/PeerProvider";
+
+// Set the app element for react-modal
+Modal.setAppElement('#root');
 
 const CreateRoom = () => {
   const socket = useSocket();
-  const { offerCreated } = usePeer();
   const [email, setEmail] = useState(
     (localStorage.getItem("loggedUser") &&
       JSON.parse(localStorage.getItem("loggedUser")).email) ||
       ""
   );
   const [room, setRoom] = useState("");
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [copyMessage, setCopyMessage] = useState("");
-  const [isCreatingRoom, setIsCreatingRoom] = useState(false);
-
+  const [showModal, setShowModal] = useState(false);
+  const [copied, setCopied] = useState(false);
   const navigate = useNavigate();
 
   const generateRandomString = () => {
@@ -47,97 +44,105 @@ const CreateRoom = () => {
   const createRoom = useCallback(() => {
     const inputString = generateRandomString();
     const formattedString = formatString(inputString);
-    setRoom(formattedString);
+    setRoom(formattedString); // Set the room state with the formatted room string
     return formattedString;
   }, []);
 
-  const handleJoinRoom = () => {
-    setIsCreatingRoom(false);
-    setModalIsOpen(true);
+  const handleJoinRoom = useCallback(async () => {
+    if (email) {
+      if (room) {
+        socket.emit("room-join", { room, email });
+      } else {
+        const newRoom = createRoom();
+        socket.emit("room-join", { room: newRoom, email });
+        setShowModal(true); // Show modal with the new room information
+      }
+    }
+  }, [createRoom, email, room, socket]);
+
+  const handleroomJoined = (data) => {
+    setRoom(data);
+    navigate(`/${data}`);
   };
 
-  const handleCreateRoom = () => {
-    setIsCreatingRoom(true);
-    const roomToCreate = createRoom();
-    socket.emit("room-join", { room: roomToCreate, email });
-    setModalIsOpen(true);
-  };
+  useEffect(() => {
+    socket.on("joined-room", handleroomJoined);
+    return () => {
+      socket.off("joined-room", handleroomJoined);
+    };
+  }, [socket]);
 
-  const handleRoomSubmit = () => {
-    navigate(`/${room}`);
-  };
-
-  const copyToClipboard = () => {
-    navigator.clipboard.writeText(room);
-    setCopyMessage("Copied!");
-    setTimeout(() => {
-      setCopyMessage("");
-    }, 2000);
+  const handleCopy = () => {
+    navigator.clipboard.writeText(room).then(() => {
+      setCopied(true);
+    });
   };
 
   const closeModal = () => {
-    setModalIsOpen(false);
-    setCopyMessage("");
+    setShowModal(false);
+    setCopied(false);
   };
 
   return (
-    <div className="w-full h-screen flex flex-col items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600 text-white">
-      <h1 className="text-4xl font-bold mb-8">Video Conference</h1>
-      <button
-        onClick={handleJoinRoom}
-        className="px-8 py-4 bg-white text-black font-bold rounded-lg shadow-md hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50 mb-4"
-      >
-        Join Room
-      </button>
-      <button
-        onClick={handleCreateRoom}
-        className="px-8 py-4 bg-white text-black font-bold rounded-lg shadow-md hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50"
-      >
-        Create Room
-      </button>
+    <div className="w-full h-screen flex flex-col items-center justify-center bg-slate-300 p-4">
+      <h1 className="text-4xl font-bold mb-6">Video Conference</h1>
+      <div className="mb-4 w-full max-w-md">
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter your email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          className="w-full p-2 mb-4 border border-gray-300 rounded-md"
+          disabled
+        />
+        <button
+          onClick={() => {
+            createRoom();
+            setShowModal(true);
+          }}
+          className="w-full p-2 bg-green-500 text-white rounded-md hover:bg-blue-600 mb-4"
+        >
+          Create Room
+        </button>
+        <div className="flex items-center justify-center mb-4">
+          <span className="mx-2 text-2xl text-slate-600">or</span>
+        </div>
+        <input
+          type="text"
+          name="room"
+          placeholder="Enter your room"
+          onChange={(e) => setRoom(e.target.value)}
+          value={room}
+          className="w-full p-2 mb-4 border border-gray-300 rounded-md"
+        />
+        <button
+          onClick={handleJoinRoom}
+          className="w-full p-2 bg-green-500 text-white rounded-md hover:bg-green-600"
+        >
+          Join Room
+        </button>
+      </div>
 
       <Modal
-        isOpen={modalIsOpen}
+        isOpen={showModal}
         onRequestClose={closeModal}
-        className="flex flex-col items-center justify-center bg-white text-black rounded-lg p-8 shadow-lg"
+        contentLabel="Room Created"
+        className="flex flex-col items-center justify-center bg-white p-4 rounded-md"
         overlayClassName="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center"
-        contentLabel="Room Modal"
       >
-        {isCreatingRoom ? (
-          <>
-            <h2 className="text-2xl font-bold mb-4">Room Created!</h2>
-            <p className="mb-4">Room Code:</p>
-            <div className="flex items-center space-x-2 mb-8">
-              <input
-                type="text"
-                value={room}
-                readOnly
-                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
-              />
-              <button
-                onClick={copyToClipboard}
-                className="text-blue-600 hover:text-blue-800"
-              >
-                <FiCopy size={24} />
-              </button>
-            </div>
-            {copyMessage && <p className="text-green-600">{copyMessage}</p>}
-          </>
-        ) : (
-          <>
-            <h2 className="text-2xl font-bold mb-4">Join a Room</h2>
-            <p className="mb-4">Enter Room Code:</p>
-            <input
-              type="text"
-              value={room}
-              onChange={(e) => setRoom(e.target.value)}
-              className="w-full px-4 py-2 mb-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
-            />
-          </>
-        )}
+        <h2 className="text-2xl font-bold mb-4">Room Created</h2>
+        <p className="mb-4">Room ID: {room}</p>
+        <p className="mb-4 text-orange-500">Share The roomID with your student </p>
         <button
-          onClick={handleRoomSubmit}
-          className="px-4 py-2 bg-blue-600 text-black font-bold rounded-lg hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50"
+          onClick={handleCopy}
+          className="p-2 bg-gray-300 text-black rounded-md hover:bg-gray-400 mb-4"
+        >
+          {copied ? "Copied!" : "Copy Room ID"}
+        </button>
+        <button
+          onClick={handleJoinRoom}
+          className="p-2 bg-blue-500 text-white bg-green-400 rounded-md hover:bg-blue-600"
         >
           Go to Room
         </button>
